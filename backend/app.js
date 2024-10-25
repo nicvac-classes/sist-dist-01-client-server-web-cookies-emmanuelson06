655318514174ee0fd6584fa385f9cd94b5f6e93d
emmanuelson06 <148054057+emmanuelson06@users.noreply.github.com>
2024-10-25 12:17:24 +0000

    Esercizio finito

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser()); //Da aggiungere
+
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name; // Legge il cookie "name"
+    if (name) {
+        // Se il cookie esiste, mostra la pagina di saluto
+        res.render('greet', { message:'Bentornato', name: name });
+    } else {
+        // Se non esiste, mostra il form
+        res.render('form');
+    }
 });
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    // Imposta un cookie chiamato "name" con valore l'input dell'utente
+    res.cookie('name', name, { maxAge: 24 * 60 * 60 * 1000 }); 
+    res.render('greet', { message:'Benvenuto', name: name });
+});
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('name'); // Cancella il cookie chiamato "name"
+    res.redirect('/'); // Reindirizza alla home page
 });
 
 const PORT = 3000;
